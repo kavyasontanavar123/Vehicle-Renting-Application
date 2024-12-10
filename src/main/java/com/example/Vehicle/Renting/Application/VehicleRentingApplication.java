2b67bd735da3e2c566d4ce19f7031fcb814197c7
kavyasontanavar123 <kavyasontanavar@gmail.com>
2024-12-10 15:34:26 +0530

    creating vehicle renting application project

---
+++ src/main/java/com/example/Vehicle/Renting/Application/VehicleRentingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vehicle.Renting.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentingApplication.class, args);
+	}
+
+}
