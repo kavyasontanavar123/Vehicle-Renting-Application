545985a0c2f2b03b3b7b4e3db7e9a0c6c6d40f0d
kavyasontanavar123 <kavyasontanavar@gmail.com>
2024-12-11 13:51:38 +0530

    creating registration

--- src/main/java/com/example/Vehicle/Renting/Application/controller/UserController.java
+++ src/main/java/com/example/Vehicle/Renting/Application/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.example.Vehicle.Renting.Application.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Vehicle.Renting.Application.entity.User;
 import com.example.Vehicle.Renting.Application.service.UserService;
+import com.example.Vehicle.Renting.Application.util.ResponseStructure;
 
 @RestController
 public class UserController {
@@ -12,6 +18,15 @@ public class UserController {
 		super();
 		this.userService = userService;
 	}
+	@PostMapping("/register")
+	public ResponseEntity<ResponseStructure<User>>registration(@RequestBody User user){
+		user= userService.registration(user);
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(ResponseStructure.create(HttpStatus.CREATED.value(),"customer created",user));
+		
+	}
+		
 	
 	
 
